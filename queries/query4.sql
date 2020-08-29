2f8c9f508b1f7131d33bec8ea6019987b5ea5bff
Vojtech Lengal <vojta.lengal@gmail.com>
2020-08-29 22:35:46 +0200

    4th query slightly changed

--- queries/query4.sql
+++ queries/query4.sql
@@ -1,6 +1,6 @@
--- vyber všechny kategorie, které obsahují aspoň 1 produkt s cenou alespoň 1000 Kč
+-- vyber všechny kategorie s průměrnou cenou produktů alespoň 750 Kč
 SELECT c.* FROM Category c
 INNER JOIN Product p
 ON c.Id = p.IdCategory
 GROUP BY c.Id, c.Name
-HAVING MAX(p.Price) >= 1000
+HAVING AVG(p.Price) >= 750
