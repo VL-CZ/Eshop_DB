116baba0f6cf7b993e17ed667fe7da0e4ef36f3b
Vojtech Lengal <vojta.lengal@gmail.com>
2020-08-31 21:23:26 +0200

    2nd query slightly changed

--- queries/query2.sql
+++ queries/query2.sql
@@ -1,4 +1,4 @@
 -- vyber všechny produkty, které si nikdo nekoupil (tzn. nejsou v žádné objednávce)
 SELECT * FROM Product p 
 WHERE NOT EXISTS
-(SELECT * FROM ProductOrder po WHERE p.Id = po.IdProduct)
+(SELECT * FROM ProductOrder po WHERE po.IdProduct = p.Id)
